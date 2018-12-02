7dd038cbcb9c4b620c6272a117e4b75fb175b572
Adrian Pauli <pauli.adrian96@gmail.com>
2018-12-02 17:11:40 +0100

    Setup spring boot web

---
+++ src/main/java/com/github/homespirit/hueaccesscontrol/HueAccessControlProxy.java
@@ -0,0 +1,14 @@
+package com.github.homespirit.hueaccesscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HueAccessControlProxy {
+
+
+    public static void main(String args[]) {
+        SpringApplication.run(HueAccessControlProxy.class, args);
+    }
+
+}
