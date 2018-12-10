725edb317298ed75ff80daa9acab88b113336a69
Adrian Pauli <pauli.adrian96@gmail.com>
2018-12-10 14:20:36 +0100

    Ensure parital PUTs are supported

--- src/main/java/com/github/homespirit/hueaccesscontrol/common/AppConfig.java
+++ src/main/java/com/github/homespirit/hueaccesscontrol/common/AppConfig.java
@@ -1,5 +1,7 @@
 package com.github.homespirit.hueaccesscontrol.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.homespirit.hueaccesscontrol.GroupsConfig;
 import com.github.homespirit.hueaccesscontrol.common.api.HueRestTemplate;
 import com.github.homespirit.hueaccesscontrol.common.config.BridgeConfig;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
@@ -32,7 +36,33 @@ public class AppConfig {
 
     @Bean
     public HueRestTemplate hueRestTemplate(BridgeConfig bridgeConfig, CurrentUserSupplier currentUserSupplier) {
-        return new HueRestTemplate(bridgeConfig, currentUserSupplier, new RestTemplate());
+        var restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(0, createHueRestMessageConverter());
+        return new HueRestTemplate(bridgeConfig, currentUserSupplier, restTemplate);
+    }
+
+    @Autowired
+    public void configureObjectMapper(ObjectMapper mapper) {
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter jsonConverter(ObjectMapper mapper) {
+        var converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(mapper);
+        return converter;
+    }
+
+    private ObjectMapper createHueRestObjectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return objectMapper;
+    }
+
+    private HttpMessageConverter createHueRestMessageConverter() {
+        var jacksonConverter = new MappingJackson2HttpMessageConverter();
+        jacksonConverter.setObjectMapper(createHueRestObjectMapper());
+        return jacksonConverter;
     }
 
 }
