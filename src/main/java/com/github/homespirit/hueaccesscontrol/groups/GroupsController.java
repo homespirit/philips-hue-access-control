ca43a885a0cba81aabc4daaea5e2be16dc42e916
Adrian Pauli <pauli.adrian96@gmail.com>
2018-12-18 19:15:12 +0100

    Add groups CRUD api

--- src/main/java/com/github/homespirit/hueaccesscontrol/groups/GroupsController.java
+++ src/main/java/com/github/homespirit/hueaccesscontrol/groups/GroupsController.java
@@ -1,10 +1,14 @@
 package com.github.homespirit.hueaccesscontrol.groups;
 
 import com.github.homespirit.hueaccesscontrol.bridge.Api;
+import com.github.homespirit.hueaccesscontrol.bridge.dto.DeleteStatus;
+import com.github.homespirit.hueaccesscontrol.bridge.dto.UpdateStatus;
 import com.github.homespirit.hueaccesscontrol.groups.dto.Group;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -25,5 +29,21 @@ public class GroupsController {
     public ResponseEntity<Map<Integer, Group>> list() {
         return api.list();
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Group> read(@PathVariable int id) {
+        return api.read(id);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<List<UpdateStatus<?>>> update(@PathVariable int id, @RequestBody @Valid Group group) {
+        return api.update(id, group);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<List<DeleteStatus>> delete(@PathVariable int id) {
+        return api.delete(id);
+    }
+
 
 }
